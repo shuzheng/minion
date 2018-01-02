67a9d0af127aff10599d9f2bd8f01925ffbf49c4
shuzheng <469741414@qq.com>
2018-01-02 12:59:55 +0800

    初始化eureka

---
+++ minion-eureka/src/main/java/cn/javay/minion/eureka/MinionEurekaApplication.java
@@ -0,0 +1,14 @@
+package cn.javay.minion.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MinionEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinionEurekaApplication.class, args);
+	}
+}
