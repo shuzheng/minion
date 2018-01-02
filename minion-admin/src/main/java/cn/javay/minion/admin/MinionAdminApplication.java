c16a4c65283e2be3f11ef85d495a75133c2348a8
shuzheng <469741414@qq.com>
2018-01-02 17:01:21 +0800

    初始化admin模块

--- minion-admin/src/main/java/cn/javay/minion/admin/MinionAdminApplication.java
+++ minion-admin/src/main/java/cn/javay/minion/admin/MinionAdminApplication.java
@@ -1,17 +1,17 @@
-package cn.javay.minion.config;
+package cn.javay.minion.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class MinionConfigApplication {
+@EnableAdminServer
+public class MinionAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MinionConfigApplication.class, args);
+		SpringApplication.run(MinionAdminApplication.class, args);
 	}
 
 }
---
+++ minion-admin/src/test/java/cn/javay/minion/admin/MinionAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.javay.minion.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinionAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
